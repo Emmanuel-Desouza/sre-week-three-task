20ac187054627a8cb64a69fa7ab71b086233e7ec
Emmanuel-Desouza <116979770+Emmanuel-Desouza@users.noreply.github.com>
2024-04-22 06:25:07 +0000

    update

---
+++ README.md
@@ -0,0 +1,9 @@
+Task 1: Solution in the watcher.sh file
+
+Task 2: Potential solutions and tools to reduce toil in the ticketing system. 
+
+Automate workflow to implement solution in the watcher file.
+
+Automate a workflow for an alerting and ticketing system that intelligently detects false-positives and creates a ticket for noisiest alerts with priority labels. PagerDuty and Jira can be used in this automated workflow.
+
+Implement and alerting system that groups related incidents and alert on them. This makes it possible for such similar incidents to be resolved by a fix.
--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,27 @@
-#TODO
+#!/bin/bash
+
+# namespace name
+NAMESPACE="sre"
+
+# deployment name
+DEPLOYMENT="swype-app"
+
+# Maximum restart count before downscaling
+MAX_RESTARTS=4
+
+while true; do
+  # Get the number of restarts of the pod
+  RESTARTS=$(kubectl get pods -n ${NAMESPACE} -l app=${DEPLOYMENT} -o jsonpath="{.items[0].status.containerStatuses[0].restartCount}")
+
+  echo "Current restarts count: ${RESTARTS}"
+
+  # If restart count exceeds maximum restart count, downscale the deployment
+  if (( RESTARTS > MAX_RESTARTS )); then
+    echo "Maximum restart count exceeded. Downscaling deployment..."
+    kubectl scale --replicas=0 deployment/${DEPLOYMENT} -n ${NAMESPACE}
+    break
+  fi
+
+  # Break before next check cycle
+  sleep 60
+done
